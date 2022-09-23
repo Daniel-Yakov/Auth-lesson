9d9027c68bcf500bc036b57699e87cf5cbc3aa1b
Daniel Yakov <daniel577000@gmail.com>
2022-09-23 12:52:52 +0300

    Add passport.js

--- app.js
+++ app.js
@@ -4,18 +4,25 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-//const encrypt = require("mongoose-encryption");
-//const md5 = require("md5"); // hash encryption
-const bcrypt = require('bcrypt'); // bycrypt hash encryption
-const saltRounds = 10;
-
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/userDB");
 
@@ -24,10 +31,14 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-// Add encryption to the 'password'
-//userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 app.get("/", function(req, res){
@@ -44,23 +55,19 @@ app.route("/login")
     })
 
     .post(function(req, res){
-        const userName = req.body.username;
-        const password = req.body.password;
 
-        // Checking if the userName exists in the DB
-        User.findOne({email: userName}, function(err, foundUser){
+        const user = new User({
+            username: req.body.username,
+            password: req.body.password
+        });
+
+        req.login(user, function(err){
             if (err){
                 console.log(err);
             } else {
-                // The userName indeed exists in the DB
-                if (foundUser){
-                    // iF the password typed matches the password in DB, login.
-                    bcrypt.compare(password, foundUser.password, function(err, result) {
-                        if(result === true){
-                            res.render("secrets");
-                        }
-                    });
-                }
+                passport.authenticate("local")(req, res, function(){
+                    res.redirect("/secrets");
+                });
             }
         });
     });
@@ -78,31 +85,39 @@ app.route("/register")
 
     .post(function(req, res){
 
-        // This will encrypt the password with the bcrypt encryption
-        bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-            // Create the new user date
-            const newUser = new User({
-                email: req.body.username,
-                password: hash
-            })
-            // Add the data to userDB
-            .save(function(err){
-                if(err){
-                    console.log(err);
-                } else {
-                    res.render("secrets");
-                }
-            });
+        User.register({username: req.body.username}, req.body.password, function(err, user){
+            if(err){
+                console.log(err);
+                res.redirect("/register");
+            } else {
+                passport.authenticate("local")(req, res, function(){
+                    res.redirect("/secrets");
+                });
+            }
         });
+
     });
 
 // End of 'register' route //
 
+app.get("/secrets", function(req, res){
+    if (req.isAuthenticated()){
+        res.render("secrets");
+    } else {
+        res.redirect("/login");
+    }
+});
 
 
-
-
-
+app.get("/logout", function(req, res){
+    req.logout(function(err){
+        if (err){
+            console.log(err);
+        } else {
+            res.redirect("/");
+        }
+    });
+});
 
 
 
